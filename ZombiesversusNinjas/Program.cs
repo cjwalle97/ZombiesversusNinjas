be1f35dc5aa1dc3925f69a20b93fc4992182e6ab
cjwalle97 <cjwalle97@gmail.com>
2017-01-04 17:29:12 -0600

    initial commit
    Initial commit

---
+++ ZombiesversusNinjas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZombiesversusNinjas
+{
+    class Program
+    {
+        class Entity
+        {
+            private int Health;
+            private int Attack;
+            private string Name;
+            protected int health;
+            protected int attack;
+            Entity() { }
+            Entity(int h, int att)
+            {
+                Health = h;
+                Attack = att;
+            }
+            public virtual bool ATTACK(Entity);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
